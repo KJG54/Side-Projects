393de07832244c3b18f500dc244a56c4ae6862f6
Krystian Garcia <Krystian.Garcia@Gmail.com>
2022-12-19 16:01:10 -0700

    testing

---
+++ extra-assessment/src/main/java/com/techelevator/Module1CodingAssessment.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Module1CodingAssessment {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+
+	}
+
+}
